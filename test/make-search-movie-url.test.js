52140e59ff08c353b0a0ba1c378adf808d1b4031
Ben Parisot <me@benparisot.com>
2019-03-05 15:37:31 -0800

    set api key as standalone variable to be set in url

--- test/make-search-movie-url.test.js
+++ test/make-search-movie-url.test.js
@@ -1,8 +1,10 @@
 const test = QUnit.test;
-const searchMovieURL = 'https://api.themoviedb.org/3/movie/550?api_key=9b04e9b50b5df524106f0c1b348dbfb5';
+const searchMovieURL = 'https://api.themoviedb.org/3/movie/550?';
+const apiKey = '9b04e9b50b5df524106f0c1b348dbfb5';
 
 function makeSearchMovieURL(queryOptions) {
     const url = new URL(searchMovieURL);
+    url.searchParams.set('api_key', apiKey);
     url.searchParams.set('language', 'en-us');
     url.searchParams.set('include_adult', false);
     url.searchParams.set('query', queryOptions.searchTerm);
