d787762bd837a52d1ff6bea7d7d185f405ef5d09
Ben Parisot <me@benparisot.com>
2019-03-05 15:02:54 -0800

    update form input text when url search term changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import movies from '../data/movies.js';
 import loadList from './list-component.js';
 import './pagination-component.js';
-import './search-component.js';
+import { updateSearchField } from './search-component.js';
+import { readFromQuery } from './hash-query.js';
 
+window.addEventListener('hashchange', () => {
+    const query = window.location.hash.slice(1);
+    const queryOptions = readFromQuery(query);
+    console.log(queryOptions);
+    updateSearchField(queryOptions.searchTerm);
+});
 loadList(movies);
--- src/search-component.js
+++ src/search-component.js
@@ -1,6 +1,7 @@
 import { searchToQuery } from './hash-query.js';
 const searchFormNode = document.getElementById('search-form');
 const searchTermInput = searchFormNode.querySelector('input');
+
 searchFormNode.addEventListener('submit', event => {
     event.preventDefault();
     const searchTerm = searchTermInput.value;
@@ -8,3 +9,7 @@ searchFormNode.addEventListener('submit', event => {
     const newQuery = searchToQuery(existingQuery, searchTerm);
     window.location.hash = newQuery;
 });
+
+export function updateSearchField(searchTerm) {
+    searchTermInput.value = searchTerm;
+}
